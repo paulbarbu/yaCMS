44bfb474d835c14c5bf86169209b9a1fd03c162d
Paul <paullik@yahoo.com>
2011-05-07 22:12:14 +0300

    added information on "about you"

--- primul site complet/pages/user.php
+++ primul site complet/pages/user.php
@@ -1,7 +1,9 @@
 <p>
 <ul>
 	<?php
-	//items here
+	echo '<li> You are currently on: ' ,$_SERVER['SERVER_NAME'], '</li>' ,PHP_EOL,
+		'<li> Your IP:PORT: ' ,$_SERVER['REMOTE_ADDR'], '</li>' ,PHP_EOL,
+		'<li> Port on wich the connection was established: ' ,$_SERVER['REMOTE_PORT'], '</li>' ,PHP_EOL;
 	?>
 </ul>
 </p>
