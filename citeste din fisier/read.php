27ffd9d51058e595ea22d5327f2d43ce1c42b425
PauLLiK <paullik@yahoo.com>
2011-05-12 10:43:31 +0300

    file is now opened after the checks

--- citeste din fisier/read.php
+++ citeste din fisier/read.php
@@ -1,9 +1,10 @@
 <?php
 $path = __DIR__ . DIRECTORY_SEPARATOR . 'file.txt';
 
-$file = fopen($path, "rb");
 if(is_readable($path)){
 	if(filesize($path)){
+		$file = fopen($path, "rb");
+		
 		while(!feof($file)){
 			$bytes = rand(1, 32);
 			$content = fread($file, $bytes);
