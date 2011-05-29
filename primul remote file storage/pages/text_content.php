6a064ecf3224e5000ff2c097fc3138b5a082c4f4
PauLLiK <paullik@yahoo.com>
2011-05-29 13:14:22 +0300

    implemented VL for textarea and successfull message

--- primul remote file storage/pages/text_content.php
+++ primul remote file storage/pages/text_content.php
@@ -3,17 +3,30 @@
 if(is_numeric($feedback['text'])){
     echo '<h3>';
     switch($feedback['text']){
-        case 2: echo 'Incorrect passphrase!'; 
+        case 1: echo 'Incorrect passphrase!';
+            break;
+        case 2: echo 'Error on reading the file!';
+            break;
+        case 3: echo 'Error on writing to file!';
+        default;
     }
     echo '</h3>';
 }
+elseif(NULL != $feedback['text']['contents']){
+    echo '<label for="id-c">Edit here:</label><br /><textarea name="contents" id="id-c" rows="15" cols="100">' , $feedback['text']['contents'] , '</textarea><br />' , PHP_EOL;
+    echo '<input type="hidden" name="file" value="' , $feedback['text']['msg'] , '" />';
+}
 elseif(NULL != $feedback['text']['files']){
+
     foreach($feedback['text']['files'] as $file){
         echo '<input type="radio" name="filelist" id="id-' , $file , '" value="' , $file , '" /><label for="id-' , $file , '">' , $file , '</label><br />' , PHP_EOL; 
     }
     echo '<input type="hidden" name="sec" value="' , $feedback['text']['msg'] , '" />';
 }
-else{
+elseif(NULL != $feedback['text']['msg']){
+    echo '<h3>' , $feedback['text']['msg'] , '</h3>';
+}
+else{ //we don't have to show the file list so the user must specify the 'secret'
     echo '<label for="id-s">Passphrase</label><input type="password" name="secret" id="id-s" />';
 }
 ?>
