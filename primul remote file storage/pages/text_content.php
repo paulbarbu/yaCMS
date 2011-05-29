9442e5536bfa1b117c8c1d125a23c4248972bd42
PauLLiK <paullik@yahoo.com>
2011-05-29 14:16:29 +0300

    name of the updated file is now stylized here

--- primul remote file storage/pages/text_content.php
+++ primul remote file storage/pages/text_content.php
@@ -13,21 +13,29 @@ if(is_numeric($feedback['text'])){
     echo '</h3>';
 }
 elseif(NULL != $feedback['text']['contents']){
-    echo '<label for="id-c">Edit here:</label><br /><textarea name="contents" id="id-c" rows="15" cols="100">' , $feedback['text']['contents'] , '</textarea><br />' , PHP_EOL;
-    echo '<input type="hidden" name="file" value="' , $feedback['text']['msg'] , '" />';
+    echo '<label for="id-c">Edit here:</label><br /><textarea name="contents"
+        id="id-c" rows="15" cols="100">' , $feedback['text']['contents'] 
+        , '</textarea><br />' , PHP_EOL;
+    echo '<input type="hidden" name="file" value="'
+        , $feedback['text']['msg'] , '" />';
 }
 elseif(NULL != $feedback['text']['files']){
 
     foreach($feedback['text']['files'] as $file){
-        echo '<input type="radio" name="filelist" id="id-' , $file , '" value="' , $file , '" /><label for="id-' , $file , '">' , $file , '</label><br />' , PHP_EOL; 
+        echo '<input type="radio" name="filelist" id="id-' , $file , 
+            '" value="' , $file , '" /><label for="id-' , $file , '">'
+            , $file , '</label><br />' , PHP_EOL; 
     }
-    echo '<input type="hidden" name="sec" value="' , $feedback['text']['msg'] , '" />';
+    echo '<input type="hidden" name="sec" value="' , $feedback['text']['msg'] ,
+        '" />';
 }
 elseif(NULL != $feedback['text']['msg']){
-    echo '<h3>' , $feedback['text']['msg'] , '</h3>';
+    echo '<h3><i>' , $feedback['text']['msg'] , '</i> successfully updated!
+        </h3>';
 }
-else{ //we don't have to show the file list so the user must specify the 'secret'
-    echo '<label for="id-s">Passphrase</label><input type="password" name="secret" id="id-s" />';
+else{ //the user must specify the 'secret'
+    echo '<label for="id-s">Passphrase</label><input type="password" 
+        name="secret" id="id-s" />';
 }
 ?>
 <input type="submit" value="Edit" name="edit" />
