1695f3dacc9eedc3fef088b75dbbc2470d4c211a
PauLLiK <paullik@yahoo.com>
2011-05-30 22:39:43 +0300

    geometry changed to fit the new login system

--- primul remote file storage/pages.php
+++ primul remote file storage/pages.php
@@ -1,27 +1,37 @@
 <?php
 return array(
-	'home' => array(
-		'title' => 'Home',
-		'content' => 'home.php',
-	),
-	'upload' => array(
-	    'title' => 'Upload',
-            'content' => 'upload_content.php',
-            'preprocess' => array(
-            	'upload_const' => 'upload_const.php',
-            	'upload' => 'upload.php',
-            ), 
-	),
-	'notfound' => array(
-		'title' => 'Inexistent page',
-		'content' => 'notfound.php',
+    'home' => array(
+        'title' => 'Home',
+        'content' => 'home.php',
+    ),
+    'upload' => array(
+        'title' => 'Upload',
+        'content' => 'upload_content.php',
+        'login' => TRUE,
+        'preprocess' => array(
+            'upload_const' => 'upload_const.php',
+            'upload' => 'upload.php',
         ),
-        'text' => array(
-            'title' => 'Edit your text',
-            'content' => 'text_content.php',
-            'preprocess' => array(
-                'text_const' => 'text_const.php',
-                'text' => 'text.php',
-            ),
+    ),
+    'notfound' => array(
+        'title' => 'Inexistent page',
+        'content' => 'notfound.php',
+    ),
+    'text' => array(
+        'title' => 'Edit your text',
+        'content' => 'text_content.php',
+        'login' => TRUE,
+        'preprocess' => array(
+            'text_const' => 'text_const.php',
+            'text' => 'text.php',
         ),
+    ),
+    'login' => array(
+        'title' => 'Log In',
+        'content' => 'login_content.php',
+        'preprocess' => array(
+            'login_const' => 'login_const.php',
+            'login' => 'login.php',
+        ),
+    ),
 );
