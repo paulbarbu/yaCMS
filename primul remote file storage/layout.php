9e2684be0d4fdc765c0d819a55efdff6b39969fc
PauLLiK <paullik@yahoo.com>
2011-05-31 07:04:49 +0300

    new login/logout header

--- primul remote file storage/layout.php
+++ primul remote file storage/layout.php
@@ -7,14 +7,26 @@
 
 </head>
 <body>
-	<div id="menu">
-		<h3>Menu here</h3>
-		<?php echo build_menu_from_pages($pages, $page)?>
-	</div>
-	
-	<div id="content">
+    <div id="header">
+        <div id="login">
+            <?php
+            if(isset($_SESSION['uID'])){
+                echo '<a href="?show=logout">Log out</a>';
+            }
+            else{
+                echo '<a href="?show=login">Log in</a>';
+            }
+            ?>
+        </div>
+    </div>
+    <div id="menu">
+        <h3>Menu here</h3>
+        <?php echo build_menu_from_pages($pages, $page)?>
+    </div>
+
+    <div id="content">
         <h3><?php echo $pages[$page]['title'];?></h3>
-		<p><?php include BASE_DIR . '/pages/' . $pages[$page]['content'];?></p>
-	</div>
+        <p><?php include BASE_DIR . '/pages/' . $pages[$page]['content'];?></p>
+    </div>
 </body>
 </html>
