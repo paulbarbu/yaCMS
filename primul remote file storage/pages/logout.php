78ab34fda7b7df789561ace3cb8466c7cb37d6b3
PauLLiK <paullik@yahoo.com>
2011-05-31 07:06:21 +0300

    logout system

---
+++ primul remote file storage/pages/logout.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Log out script
+ *
+ * At first we need to unset the $_SESSION, then to delete the session cookie
+ * if it's set and finally to destroy the session itself
+ */
+
+$del_cookie = FALSE;
+$destroy_session = FALSE;
+
+$_SESSION = array();
+
+if(ini_get("session.use_cookies")){
+    $params = session_get_cookie_params();
+    $del_cookie = setcookie(session_name(), '', time() - 42000, $params['path'],
+        $params['domain'], $params['secure'], $params['httponly']
+    );
+}
+
+$destroy_session = session_destroy();
+
+return $destroy_session & $del_cookie;
