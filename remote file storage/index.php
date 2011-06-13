dc850541b2fa79e04e9bb951c75cd72aa4d08959
PauLLiK <paullik@yahoo.com>
2011-06-13 16:57:04 +0300

    replaced with the files from "primul remote file storage"

--- remote file storage/index.php
+++ remote file storage/index.php
@@ -1,18 +1,39 @@
 <?php
-//include bug => file not found
+
 require_once '.' . DIRECTORY_SEPARATOR . 'functions.php';
-require_once '.' . DIRECTORY_SEPARATOR . 'const.php';
+$pages = require_once '.' . DIRECTORY_SEPARATOR . 'pages.php';
 
-$result = NULL;
 const BASE_DIR = __DIR__;
+$feedback = array();
 
-if(isset($_POST['upload'])){
-	$result = require '.' . DIRECTORY_SEPARATOR . 'upload.php';
+if(isset($_GET['show'])){
+    if(array_key_exists($_GET['show'], $pages)){
+        $page = $_GET['show'];
+    }
+    else{
+        $page = 'notfound';
+    }
 }
 else{
-	render('layout.php');
+    $page = 'home';
 }
 
-if($result){
-	render('result.php', compact('result'));
+session_start();
+if(!isset($_SESSION['uID']) && isset($_COOKIE[session_name()]) && is_numeric($_COOKIE[session_name()])){
+        $_SESSION['uID'] = $_COOKIE[session_name()];
 }
+elseif(isset($pages[$page]['login']) && !isset($_SESSION['uID'])){
+    /**
+     * if no session is registered and 'remember me' was not checked
+     * and still the page needs authentication
+     */
+    $page = 'login';
+}
+
+if(isset($pages[$page]['preprocess'])){
+    foreach($pages[$page]['preprocess'] as $preprocessName => $preprocessFile){
+        $feedback[$preprocessName] = require BASE_DIR . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR . $preprocessFile;
+    }
+}
+
+render('layout.php', compact('page', 'feedback', 'pages'));
