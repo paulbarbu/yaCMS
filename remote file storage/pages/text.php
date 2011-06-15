046d5fb337ff6ef2e85afce2dd8c5aa3399d1c60
PauLLiK <paullik@yahoo.com>
2011-06-15 20:30:32 +0300

    modified to fit the new global constant

--- remote file storage/pages/text.php
+++ remote file storage/pages/text.php
@@ -3,20 +3,20 @@
 /**
  * Business Logic of "Edit text on-site"
  *
- * This script will return an array with available text files in the 
- * 'uploads' folder, or a string the will be laoded in a textbox from VL or a 
+ * This script will return an array with available text files in the
+ * 'uploads' folder, or a string the will be laoded in a textbox from VL or a
  * message confirming the file's change or the error that occured.
  *
  * PATH - string containing the path for list_text_files() to look in
  * $file - the file selected to be edited
  * $result['files'] - an array containing all the text files from the 'secret'
- * folder 
+ * folder
  * $result['content'] - string that holds the content of the chosen file
- * $result['msg'] - string that holds metadatas about the secret folder, the 
+ * $result['msg'] - string that holds metadatas about the secret folder, the
  * file or the upload itself
  * ERR_PASS - will be returned when passphrase(secret) is incorrect
  * ERR_READ - returned when file_get_contents() fails
- * ERR_WRITE - returned if there is an error on writing the new contents to the 
+ * ERR_WRITE - returned if there is an error on writing the new contents to the
  * file
  */
 
@@ -27,13 +27,12 @@ $result = array(
 );
 
 if(isset($_POST['edit'])){
-    
+
     if(isset($_POST['secret']) && !empty($_POST['secret'])){
         $secret = $_POST['secret'];
 
-        if(is_dir(PATH . DIRECTORY_SEPARATOR . $secret)){
-            $result['files'] = list_text_files(PATH . DIRECTORY_SEPARATOR
-                . $secret);
+        if(is_dir(PATH . $secret)){
+            $result['files'] = list_text_files(PATH . $secret);
             $result['msg'] = $secret;
         }
         else{ //passphrase incorrect(inexistent directory)
@@ -41,15 +40,14 @@ if(isset($_POST['edit'])){
         }
     }
     elseif(isset($_POST['filelist'])){
-        $file = PATH . DIRECTORY_SEPARATOR . $_POST['sec'] 
-            . DIRECTORY_SEPARATOR . $_POST['filelist'];
+        $file = PATH . $_POST['sec'] . DIRECTORY_SEPARATOR . $_POST['filelist'];
 
         $result['contents'] = file_get_contents($file);
         if(FALSE == $result['contents']){
             return ERR_READ;
         }
 
-        $result['msg'] = DIRECTORY_SEPARATOR . $_POST['sec'] 
+        $result['msg'] = DIRECTORY_SEPARATOR . $_POST['sec']
             . DIRECTORY_SEPARATOR . $_POST['filelist'];
     }
     elseif(isset($_POST['contents'])){
@@ -59,11 +57,11 @@ if(isset($_POST['edit'])){
         if(FALSE === $check){
             return ERR_WRITE;
         }
-        
+
         $name = substr(strrchr($file, DIRECTORY_SEPARATOR), 1);
         $result['msg'] = $name;
     }
-   
+
 }
 
 return $result;
