b71e4988995542d4fad0f309db41e180e607f9e0
PauLLiK <paullik@yahoo.com>
2011-06-18 00:20:01 +0300

    new structure for gbook admin

--- guestbook/pages.php
+++ guestbook/pages.php
@@ -5,16 +5,34 @@
 return array(
     'gbook' => array(
         'title' => 'G - book',
-        'content' => 'gbook_content.php', //VL depending on the state, if admin is logged in or not
+        'content' => 'gbook_content.php',
         'preprocess' => array(
             'gbook_func' => 'gbook_func.php',
             'gbook_const' => 'gbook_const.php',
             'gbook' => 'gbook.php',
-            'gbook_admin' => 'gbook_admin.php', //BL for admin page
         ),
     ),
     'notfound' => array(
         'title' => 'Inexistent page',
         'content' => 'notfound.php',
+        'show_in_menu' => FALSE,
+    ),
+    'gbook_admin' => array(
+        'title' => 'Admin panel',
+        'content' => 'gbook_admin_content.php',
+        'preprocess' => array(
+            'gbook_admin_const' => 'gbook_admin_const.php',
+            'gbook_admin' => 'gbook_admin.php',
+            'gbook_controls' => 'gbook_controls.php'
+        ),
+        'show_in_menu' => FALSE,
+    ),
+     'logout' => array(
+        'title' => 'Log out',
+        'content' => 'logout_content.php',
+        'preprocess' => array(
+            'logout' => 'logout.php'
+        ),
+        'show_in_menu' => FALSE,
     ),
 );
