2c05e0cf70c420f693d3125a59685625971b9554
PauLLiK <paullik@yahoo.com>
2011-06-17 17:28:37 +0300

    added gbook_func

--- guestbook/pages.php
+++ guestbook/pages.php
@@ -7,6 +7,7 @@ return array(
         'title' => 'G - book',
         'content' => 'gbook_content.php', //VL depending on the state, if admin is logged in or not
         'preprocess' => array(
+            'gbook_func' => 'gbook_func.php',
             'gbook_const' => 'gbook_const.php',
             'gbook' => 'gbook.php',
             'gbook_admin' => 'gbook_admin.php', //BL for admin page
