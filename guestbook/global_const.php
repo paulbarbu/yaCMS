808246033959ed1951cbe3be1c24eca78d404b02
PauLLiK <paullik@yahoo.com>
2011-06-18 15:30:35 +0300

    path to gbook files

--- guestbook/global_const.php
+++ guestbook/global_const.php
@@ -5,3 +5,5 @@
 
 define('PATH_GBOOK', BASE_DIR . DIRECTORY_SEPARATOR . 'gbook_files'
     . DIRECTORY_SEPARATOR);
+define('PATH_MSG_FILE', PATH_GBOOK . 'msg.json');
+define('PATH_BAN_FILE', PATH_GBOOK . 'bans');
