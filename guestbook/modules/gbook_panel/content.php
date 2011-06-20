e19fb93c5b80062ea66f52325e5a9192a328d78d
PauLLiK <paullik@yahoo.com>
2011-06-20 19:42:50 +0300

    renamed the old files

---
+++ guestbook/modules/gbook_panel/content.php
@@ -0,0 +1,78 @@
+<?php
+if(!isset($_SESSION['id'])){
+    header("Location: index.php?show=login_admin");//redirecting to login module
+}
+else{
+?>
+<span id="adminlogin"><a href="index.php?show=logout">Log out</a></span>
+<h3>Ban IPs:</h3>
+<form action="" method="post">
+<?php
+    $ips = get_ips_ban();
+
+    if(is_numeric($ips)){
+        echo '<h3>';
+        switch($ips){
+            case ERR_OPEN: echo 'Error opening file! - ', ERR_OPEN;
+                break;
+            case ERR_DECODE: echo 'Message cannot be decoded! - ', ERR_DECODE;
+                break;
+            case ERR_EMPTY: echo 'No posts! - ', ERR_EMPTY;
+                break;
+            default;
+        }
+        echo '</h3>';
+    }
+    else{
+        foreach($ips as $ip){
+            echo $ip;
+        }
+    }
+?>
+<input type="submit" name="ban_ip" value="Ban selected IPs" />
+<br />
+<h3>Unban IPs:</h3>
+<?php
+    $ips = get_ips_unban();
+
+    if(is_numeric($ips)){
+        echo '<h3>';
+        switch($ips){
+            case ERR_OPEN: echo 'Error opening file! - ', ERR_OPEN;
+                break;
+            case ERR_EMPTY: echo 'No bans! - ', ERR_EMPTY;
+                break;
+            default;
+        }
+        echo '</h3>';
+    }
+    else{
+        foreach($ips as $ip){
+            echo $ip;
+        }
+    }
+
+?>
+<input type="submit" name="unban_ip" value="Unban selected IPs" />
+</form>
+<?php
+    if(is_numeric($feedback['gbook_panel'])){
+        echo '<h3>';
+
+        switch($feedback['gbook_panel']){
+            case ERR_NO_IP: echo 'Please select an IP! - ', ERR_NO_IP;
+                break;
+            case ERR_FOPEN_BAN_FILE: echo 'Could not access ban DB! - ' , ERR_FOPEN_BAN_FILE;
+                break;
+            case UNBANNED: echo 'Selected IPs successfully unbanned!';
+                break;
+            case ERR_INVALID_ARRAY: echo 'Invalid IP list! - ' , ERR_INVALID_ARRAY;
+                break;
+            case BANNED: echo 'Selected IPs successfully banned!';
+            default;
+        }
+
+        echo '</h3>';
+    }
+}
+?>
---
+++ guestbook/modules/gbook_panel/panel.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Controls for Admin panel
+ */
+
+//BAN
+if(isset($_POST['ban_ip'])){
+    if(isset($_POST['ips'])){
+        $ips = $_POST['ips'];
+
+        $fh = fopen(PATH_BAN_FILE, "a");
+
+        if(FALSE == $fh){
+            return ERR_FOPEN_BAN_FILE;
+        }
+
+        $ban = ban_ip($fh, $ips);
+
+        fclose($fh);
+
+        if(TRUE != $ban){
+            return $ban;
+        }
+        else{
+            return BANNED;
+        }
+    }
+    else{
+        return ERR_NO_IP;
+    }
+}
+
+//UNBAN
+if(isset($_POST['unban_ip'])){
+    if(isset($_POST['unban_ips'])){
+        $ips = $_POST['unban_ips'];
+
+        $bans = file(PATH_BAN_FILE);
+
+        if(FALSE == $bans){
+            return ERR_FOPEN_BAN_FILE;
+        }
+
+        for($i=0;$i<count($bans);$i++){
+            $bans[$i] = trim($bans[$i]);
+        }
+
+        $ips = array_unique(array_diff($bans, $ips)); //remove the IPs that match
+
+        $fh = fopen(PATH_BAN_FILE, "w");
+
+        if(FALSE == $fh){
+            return ERR_FOPEN_BAN_FILE;
+        }
+
+        $unban = ban_ip($fh, $ips); //write the remaining IPs
+
+        fclose($fh);
+
+        if(TRUE != $unban){
+            return $unban;
+        }
+        else{
+            return UNBANNED;
+        }
+    }
+    else{
+        return ERR_NO_IP;
+    }
+}
+
+return TRUE;
