2a8801f36f1c310450b31c687c1b2e33ccfc97f7
PauLLiK <paullik@yahoo.com>
2011-06-22 00:12:25 +0300

    feedback for pre-proc

--- guestbook/index.php
+++ guestbook/index.php
@@ -8,6 +8,7 @@ require_once BASE_DIR . 'global_const.php';
 $modules = require_once BASE_DIR . 'modules.php';
 
 $feedback = array();
+$feedback_pre = array();
 $rendered = NULL;
 
 if(isset($_GET['show'])){
@@ -22,13 +23,13 @@ else{
     $module = 'home'; //default module
 }
 if(isset($modules[$module]['pre-process']) && !empty($modules[$module]['pre-process'])){
-    foreach($modules[$module]['pre-process'] as $pre){
+    foreach($modules[$module]['pre-process'] as $pre_key => $pre){
         if(FALSE != stristr($pre, '.php')){
-            require MODULES_ROOT . $module . DIRECTORY_SEPARATOR . $pre;
+            $feedback_pre[$pre_key] = require_once MODULES_ROOT . $module . DIRECTORY_SEPARATOR . $pre;
         }
         else{//our module has another module as pre-dependency
-            foreach($modules[$pre]['pre-process'] as $dep_pre){
-                require MODULES_ROOT . $pre . DIRECTORY_SEPARATOR . $dep_pre;
+            foreach($modules[$pre]['pre-process'] as $dep_pre_key => $dep_pre){
+                $feedback_pre[$dep_pre_key] = require_once MODULES_ROOT . $pre . DIRECTORY_SEPARATOR . $dep_pre;
 
             }
         }
@@ -41,7 +42,7 @@ if(isset($modules[$module]['BL'])){
     }
 }
 
-$rendered = render('layout.php', compact('module', 'feedback', 'modules'));
+$rendered = render('layout.php', compact('module', 'feedback', 'modules', 'feedback_pre'));
 
 switch($rendered){
     case RENDER_ERR_NO_FILE: echo 'No page to display! - ' , RENDER_ERR_NO_FILE;
@@ -55,11 +56,11 @@ switch($rendered){
 if(isset($modules[$module]['post-process']) && !empty($modules[$module]['post-process'])){
     foreach($modules[$module]['post-process'] as $post){
         if(FALSE != stristr($post, '.php')){
-            require MODULES_ROOT . $module . DIRECTORY_SEPARATOR . $post;
+            require_once MODULES_ROOT . $module . DIRECTORY_SEPARATOR . $post;
         }
         else{//our module has another module as post-dependency
             foreach($modules[$post]['post-process'] as $dep_post){
-                require MODULES_ROOT . $post . DIRECTORY_SEPARATOR . $dep_post;
+                require_once MODULES_ROOT . $post . DIRECTORY_SEPARATOR . $dep_post;
 
             }
         }
