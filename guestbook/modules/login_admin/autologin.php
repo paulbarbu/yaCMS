d9e86b3879a2ca25d0448e38d9ec114961e5fb19
PauLLiK <paullik@yahoo.com>
2011-06-21 16:06:15 +0300

    checking if session already started

--- guestbook/modules/login_admin/autologin.php
+++ guestbook/modules/login_admin/autologin.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
+if(!isset($_SESSION)){
+    session_start();
+}
 
 if(!isset($_SESSION['id']) && isset($_COOKIE[session_name()]) && is_numeric($_COOKIE[session_name()])){
         $_SESSION['id'] = $_COOKIE[session_name()];
 }
-
