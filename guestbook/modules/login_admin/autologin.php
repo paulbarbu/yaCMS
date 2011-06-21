72472aa198d3b5878d0c417e8d04873b69c12962
PauLLiK <paullik@yahoo.com>
2011-06-22 00:16:06 +0300

    removed old un-prefixed files

--- guestbook/modules/login_admin/autologin.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!isset($_SESSION)){
-    session_start();
-}
-
-if(!isset($_SESSION['id']) && isset($_COOKIE[session_name()]) && is_numeric($_COOKIE[session_name()])){
-        $_SESSION['id'] = $_COOKIE[session_name()];
-}
--- guestbook/modules/login_user/autologin.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!isset($_SESSION)){
-    session_start();
-}
-
-if(!isset($_SESSION['uID']) && isset($_COOKIE[session_name()]) && is_numeric($_COOKIE[session_name()])){
-        $_SESSION['uID'] = $_COOKIE[session_name()];
-}
-
