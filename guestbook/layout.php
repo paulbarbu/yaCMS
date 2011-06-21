8e636a8ff94a6f28307815d2983179609cb9e764
PauLLiK <paullik@yahoo.com>
2011-06-21 15:59:32 +0300

    login/out link

--- guestbook/layout.php
+++ guestbook/layout.php
@@ -8,18 +8,18 @@
 </head>
 <body>
     <div id="header">
-<!--
+
         <div id="login">
             <?php
             if(isset($_SESSION['uID'])){
-                echo '<a href="?show=logout">Log out</a>';
+                echo '<a href="?show=logout_user">Log out</a>';
             }
             else{
-                echo '<a href="?show=login">Log in</a>';
+                echo '<a href="?show=login_user">Log in</a>';
             }
             ?>
         </div>
---!>
+
     </div>
     <div id="menu">
         <h3>Menu here</h3>
@@ -28,8 +28,7 @@
 
     <div id="content">
         <h3><?php echo $modules[$module]['VL']['title'];?></h3>
-        <p><?php include BASE_DIR . DIRECTORY_SEPARATOR . 'modules'
-        . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR
+        <p><?php include MODULES_ROOT . $module . DIRECTORY_SEPARATOR
         . $modules[$module]['VL']['content'];?></p>
     </div>
 </body>
