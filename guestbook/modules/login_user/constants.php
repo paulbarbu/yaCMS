95e826c363eb1794070cc7b466cf5838dda80a9d
PauLLiK <paullik@yahoo.com>
2011-06-21 16:01:03 +0300

    users' login

---
+++ guestbook/modules/login_user/constants.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Defined constants for the login script
+ */
+
+const ERR_USER = 1;
+const ERR_FOPEN_USER = 2;
+const ERR_PASS = 3;
+const ERR_NO_USER = 4;
+const ERR_NO_PASS = 5;
+const ERR_SESS = 6;
+const ERR_COOKIE = 7;
+const ERR_CAPTCHA = 8;
+const ERR_NO_CODE = 9;
+const ERR_W_CODE = 10;
