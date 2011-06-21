d3884ddd9ae7e1d6a70e2197d9559e41a5c55d31
PauLLiK <paullik@yahoo.com>
2011-06-21 16:03:02 +0300

    relative path to uploads

--- guestbook/global_const.php
+++ guestbook/global_const.php
@@ -7,3 +7,5 @@ define('MODULES_ROOT', BASE_DIR . 'modules' . DIRECTORY_SEPARATOR);
 
 define('DATA_ROOT', BASE_DIR . 'data' . DIRECTORY_SEPARATOR);
 
+define('UPLOADS_ROOT', 'uploads' . DIRECTORY_SEPARATOR);
+
