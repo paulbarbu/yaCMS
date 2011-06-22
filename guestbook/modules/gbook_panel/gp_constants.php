8e4a57d3791387c9f6d0b57939ba3f80e46ee263
PauLLiK <paullik@yahoo.com>
2011-06-22 15:53:44 +0300

    removed unnecessary constants

--- guestbook/modules/gbook_panel/gp_constants.php
+++ guestbook/modules/gbook_panel/gp_constants.php
@@ -8,14 +8,6 @@ define('PATH_MSG_FILE', DATA_ROOT . DIRECTORY_SEPARATOR . 'gbook'
 define('PATH_BAN_FILE', DATA_ROOT . DIRECTORY_SEPARATOR . 'gbook'
     . DIRECTORY_SEPARATOR . 'bans');
 
-const ERR_PASS = 1;
-const ERR_READING = 2;
-const ERR_FOPEN_ADMIN = 3;
-const ERR_DIR = 4;
-const ERR_NO_PASS = 5;
-const ERR_COOKIE = 6;
-
-//Controls constnats
 const ERR_NO_IP = 7;
 const ERR_FOPEN_BAN_FILE = 8;
 const BANNED = 9;
