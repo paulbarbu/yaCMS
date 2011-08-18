0e8466191831b12b886c485fba1a1d29bebb9dc4
Starlays <florentin.lazar@gmail.com>
2011-08-18 09:31:19 +0300

    Update Fix #1

--- src/modules/upload/constants.php
+++ src/modules/upload/constants.php
@@ -2,11 +2,11 @@
 /**
  * in this file are stored constants related to the upload.php script
  * for details go to:
- * 		http://www.php.net/manual/en/features.file-upload.errors.php
+ * http://www.php.net/manual/en/features.file-upload.errors.php
  */
 
 const UP_ERR_SIZE = 1;
-const UP_ERR_PARTiAL = 2;
+const UP_ERR_PARTIAL = 2;
 const UP_ERR_NO_FILE = 3;
 const UP_ERR_NO_TMP = 4;
 const UP_ERR_NO_WRITE = 5;
@@ -16,4 +16,6 @@ const UP_ERR_NOT_UPLOADED = 8;
 const UP_ERR_CREATE_DIR = 9;
 const UP_ERR_MOVE = 10;
 const UP_SUCCESS = 11;
-const UP_ERR_CONTACT_ADMIN = 12;
+const UP_ERR_WRITE_UPLD_DIR = 12;
+const UP_ERR_WRITE_BASE_DIR = 14;
+const UP_ERR_WRITE_FL_UPLD_DIR = 15;
--- src/modules/upload/upload.php
+++ src/modules/upload/upload.php
@@ -5,10 +5,10 @@
  * $_POST['secret'] - string that will represent the directory name to upload to
  * $file - "abbreviation" from $FILES['file']
  * $uploadDir - string that will represent the path for the user's directory
- *		specified by $_POST['secret']
+ * specified by $_POST['secret']
  * $created - check-variable to verify if the directory was created successfully
  * $moved - check-variable to verify if the intendet file was moved in unser's
- * 		directory
+ * directory
  */
 
 //create short variables
@@ -24,12 +24,12 @@ if(isset($_POST['upload'])){
             if(isset($_POST['secret']) && !empty($_POST['secret'])){ //the directory is a "must"
                 if(is_dir($uploadDir)){
                     if(!is_writable($uploadDir)){
-                        return UP_ERR_CONTACT_ADMIN;
+                        return UP_ERR_WRITE_UPLD_DIR;
                     }
                 }
                 else{
                     if(!is_writable(BASE_DIR)){
-                        return UP_ERR_CONTACT_ADMIN;
+                        return UP_ERR_WRITE_BASE_DIR;
                     }
                     umask(0003);
                     if(!mkdir($uploadDir,0774)){
@@ -42,6 +42,11 @@ if(isset($_POST['upload'])){
                     umask(0003);
                     if(!mkdir($uploadDir, 0774)){
                         return UP_ERR_CREATE_DIR;
+                    }
+                }
+                else {
+                    if(!is_writable($uploadDir)){
+                        return UP_ERR_WRITE_FL_UPLD_DIR;
                     }
                 }
 
